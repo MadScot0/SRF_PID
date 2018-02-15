264f440dfeba77223bec20b277c3628402e9b64c
MadScot0 <35280184+MadScot0@users.noreply.github.com>
2018-02-14 19:14:44 -0800

    Update SRF_PID.java
    moved sytem.print, fixed divide by zero error, made min and max default values

--- SRF_PID.java
+++ SRF_PID.java
@@ -11,49 +11,57 @@ public class SRF_PID { //v1.1
 	static double errorSum = 0, lastError = 0;
 	static double setpoint;
 	static boolean reversed = false;
-	static double max, min;
+	static double max = 1, min = -1;
 	static double lastTime = 0;
 
-	public static void setLimits(double high, double low) //this method sets the output limits for this PID object
+	public static void setLimits(double high, double low)
 	{
 		max = high;
 		min = low;
 	}
 	
-	public static void setReverse(boolean reverse) //method to reverse output values
+	public static void setReverse(boolean reverse)
 	{
 		reversed = reverse;
 	}
 	
-	public static void setPID(double P, double I, double D) //method to set values of constants in the PID loop
+	public static void setPID(double P, double I, double D)
 	{
 		kP = P;
 		kI = I;
 		kD = D;
 	}
 	
-	public static void adjustPID(double adjustP, double adjustI, double adjustD) //method so that people can incrementally adjust
-	{									//PID constants
+	public static void adjustPID(double adjustP, double adjustI, double adjustD)
+	{
 		kP+=adjustP;
 		kI+=adjustI;
 		kD+=adjustD;
 	}
 	
-	public static void setSetpoint(double target) //set the setpoint that the PID will approach; the vagueness allows for many
-	{						//applications (e.g. speed, position, etc.)
+	public static void setSetpoint(double target)
+	{
 		setpoint = target;
 	}
 	
-	public static double computePID(double current, double timeNow) //the method to compute what the output of the PID control should
-	{								//should be, current is the value of the sensor that your setpoint
-		double output;						//is compared to; timeNow just needs to be a refernece to a steady 
-		double error;						//change, DON'T PAUSE THE TIMER
+	public static double computePID(double current, double timeNow)
+	{
+		double output;
+		double error;
 		double dT = timeNow - lastTime;
 		lastTime = timeNow;
 		
+		if(timeNow == 0)
+			errorSum = 0;
+		
 		error = setpoint - current;
 		errorSum+=error;
-		output = (kP * error) + (kI * dT * errorSum) + kD / dT * (error - lastError); //Mo is typically not relevant in current output computation
+		//System.out.println("("+kP+"*"+error+")+("+kI+"*"+dT+"*"+errorSum+")+"+kD+"/"+dT+"*("+error+"-"+lastError+")");
+		if(dT != 0)
+			output = (kP * error) + (kI * dT * errorSum) + kD / dT * (error - lastError); //Mo is typically not relevant in current output computation
+		else
+			output = (kP * error) + (kI * dT * errorSum);
+		
 		
 		if(reversed)
 			output*=-1;
@@ -62,7 +70,7 @@ public class SRF_PID { //v1.1
 			output = max;
 		else if(output < min)
 			output = min;
-		
+		//System.out.println("="+output);
 		lastError = error; //Go here for math basis
 							//https://library.automationdirect.com/methods-behind-pid-loop-control/
 		return output;
