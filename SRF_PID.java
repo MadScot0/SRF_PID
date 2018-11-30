6c050598a1840b220162738ae4b81bfad0a057c1
MadScot0 <35280184+MadScot0@users.noreply.github.com>
2018-11-30 13:22:45 -0800

    Update SRF_PID.java
    began adding control and setting up undo functionality. Also added a lot of comments to explain to future team members what the heck we're doing.

--- SRF_PID.java
+++ SRF_PID.java
@@ -4,18 +4,23 @@ import edu.wpi.first.wpilibj.Joystick;
 
 public class SRF_PID { //v1.1.1
 	/*	Fixed instance overwrite problem
+	 *
+	 *  IMPORTANT: THE DEFAULT BUTTON VALUES NEED TO BE DEFINED
+	 *
 	 *  Untested
-	 * 
-	 * 
-	 * 
+	 *  Consider adding every PID into an array in a single object?
+	 *  This could be done by adding a second dimension to k and doing the equals sign part of it's definition in SRF_PID
+	 *  I'm not sure if that last part is acceptable yet
+	 *
+	 *  Also consider adding redo functionality
 	 */
 	
-	public SRF_PID(Joystick trollPID) {
+	public SRF_PID(Joystick j) {
 		
 	}
 	
-	int P = 0, I = 1, D = 2;
-	double[] k = new double[3];
+	int P = 0, I = 1, D = 2; //symbolics which can be passed into any array in which gains are stored
+	double[] k = new double[3]; //the current values of each gain, the index is based on the integers above
 	double errorSum = 0, lastError = 0;
 	double setpoint;
 	boolean reversed = false;
@@ -25,13 +30,62 @@ public class SRF_PID { //v1.1.1
 	double[] initialK = new double[3];
 	double[] mult = new double[] {1,1,1};
 	
+	//this will soon serve as the cache of old values (up to 3 in the past)
+	//the first value specifies which gain is being modified and the second denotes how many steps previous it was
+	//0 is the previous value and then it counts up from there
+	double[][] oldK = new double[3][3];
 	
+	//this is an array which stores how many 
+	int[] stored = new int[3];
+	stored[0] = -1;
+	stored[1] = -1;
+	stored[2] = -1;
+	
+	
+	//these values need to be edited to the desired default values
+	//they represent the index of the axis or whatever you're calling (e.g. control.getRawAxis(joyX))
+	int  joyX, joyY;
+	boolean reverseX = false, reverseY = false; //these will invert their respective joystick axes
+	int  applyButton, cycleGainButton, undoButton, cycleModeButton; //cycle mode toggles between set, adjust and multiply(0,1,& 2)
+	int currentMode = 0;//the mode that is changed by the cycleMode Button
+	int currentGain = 0;
+	
+	//these booleans become false when their respective button is pressed and remain so until it is realeased
+	//this means that rather then making a change for the entire duration you
+	//hold a given button, you only make a single change when it is first pressed
+	boolean letUpApply = true, letUpCycleGain = true, letUpUndo = true, letUpCycleMode = true;
+	
+	
+	//a method that will manage the cache of previous changes
+	public void updateUndo(int gain, double val)
+	{
+		//if the cache is empty then no shuffling of data is required
+		if(stored[gain] == -1)
+		{
+			stored[gain] = 0;
+			oldK[gain][0] = val;
+			return;
+		}
+		//otherwise loop and rewrite starting with 1
+		
+		
+		
+			= stored[gain]
+		
+			
+	}
+	
+	
+	//sets the maximum and minimun power values that the PID can output
+	//not having a limit may cause issues depending on what it is being inputted into
+	//default values are 1 and -1
 	public void setLimits(double high, double low)
 	{
 		max = high;
 		min = low;
 	}
 	
+	//if reversed is set to true then all future outputs from this PID will be multiplied by negative one
 	public void setReverse(boolean reverse)
 	{
 		reversed = reverse;
@@ -39,6 +93,7 @@ public class SRF_PID { //v1.1.1
 	
 	public void setPID(double nP, double nI, double nD, boolean init)
 	{
+		
 		k[P] = nP;
 		k[I] = nI;
 		k[D] = nD;
@@ -114,6 +169,55 @@ public class SRF_PID { //v1.1.1
 		mult[D]+=adjustD;
 	}
 	
+	//this sets new indices for the joysticks so that the joystick can be changed from it's default which is configured
+	//for an xBox 360 controller
+	//for the code to work proberly these two axes should be on the same joystick
+	//the booleans set whether or not that axis is inverted
+	public void configJoy(int x, boolean rX, int y, boolean rY)
+	{
+		joyX = x;
+		reverseX = rX;
+		joyY = y;
+		reverseY = rY;
+	}
 	
+	//this sets the indices of the buttons that are being 
+	public void configButtons(int apply, int undo, int cycleGain, int cycleMode)
+	{
+		applyButton = apply;
+		undoButton = undo;
+		cycleGainButton = cycleGain;
+		cycleModeButton = cycleMode;
+	}
 	
+	//This method should be called in every loop as it is what updates these methods
+	public void controlPID()
+	{
+		//cycleGain
+		if(j.getRawButton(cycleGainButton) && letUpCycleGain)
+		{
+			currentGain += 1;
+			letUpCycleGain = false;
+		}
+		else if(!j.getRawButton(cycleGainButton))
+			letUpCycleGain = true;
+			
+		
+		//cycleMode
+		if(j.getRawButton(cycleModeButton) && letUpCycleMode)
+		{
+			currentMode += 1;
+			letUpCycleMode = false;
+		}
+		else if(!j.getRawButton(cycleModeButton))
+			letUpCycleMode = true;
+		
+		//undo
+		
+		
+		//joystick
+		
+		
+		//apply
+	}
 }
